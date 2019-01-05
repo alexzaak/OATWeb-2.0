581406cf7b1262bb65a8fc3edff6bb689218a000
Alexander Zaak <alexander.zaak@gmail.com>
2019-01-05 22:58:08 +0100

    added project

---
+++ oatweb/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  items: Observable<any[]>;
+  title: String;
+  constructor(db: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.items = db.collection('test').valueChanges();
+    this.title = "Test"
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+}
